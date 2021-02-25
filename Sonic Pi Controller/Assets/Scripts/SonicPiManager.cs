5c9e3fcdaa45efafda174270c33e5560697ad5ca
mariotab28 <38128815+mariotab28@users.noreply.github.com>
2021-02-25 13:42:16 +0100

    Añadido proyecto en 2D de Unity

---
+++ Sonic Pi Controller/Assets/Scripts/SonicPiManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO: Usar enum?
+public enum PiAction {  Play, Sleep }
+
+public class SonicPiManager : MonoBehaviour
+{
+    #region Singleton Constructors
+    static SonicPiManager()
+    {
+    }
+
+    SonicPiManager()
+    {
+    }
+
+    public static SonicPiManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameObject("SonicPiManager").AddComponent<SonicPiManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    #region Member Variables
+    private static SonicPiManager _instance = null;
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// Initialize OSCHandler
+    /// </summary>
+    void Start()
+    {
+        print("Sonic Pi Manager started.");
+        OSCHandler.Instance.Init();
+    }
+
+    /// <summary>
+    /// Sends a message to Sonic Pi containing
+    /// the action's name 
+    /// </summary>
+    public void sendActionMessage(string action)
+    {
+        OSCHandler.Instance.SendMessageToClient("SonicPi", "/sonicpi/unity/trigger", action);
+       
+    }
+    #endregion
+}
---
+++ Sonic Pi Controller/Assets/Scripts/UI/BlockSelectButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BlockSelectButton : MonoBehaviour
+{
+    #region Variables
+    public string blockActionName;
+
+    public GameObject selectionMenuGO;
+
+    public GameObject blockContainerGO;
+
+    public GameObject blockPF;
+
+    #endregion
+
+    /// <summary>
+    /// ...
+    /// </summary>
+    public void SelectActionBlock()
+    {
+        // Sends the action message
+        SonicPiManager.Instance.sendActionMessage(blockActionName);
+
+        // Instantiate UI action block
+        GameObject block = Instantiate(blockPF, blockContainerGO.transform);
+        // TODO: Script para configuración de los bloques
+        block.transform.GetChild(0).GetComponent<TMPro.TMP_Text>().text = blockActionName;
+
+        // Hide selection menu 
+        selectionMenuGO.SetActive(false);
+    }
+}
